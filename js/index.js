f3c45f0aafaacd8286265d7335619b3d47573f76
Charis Theo <haristh@hotmail.com>
2018-11-06 16:19:00 +0100

    get translation from API and compare with answer

--- js/index.js
+++ js/index.js
@@ -2,39 +2,103 @@
 
 let nextBtn = document.getElementById('next-btn');
 let question = document.getElementById('question');
-let answer = document.getElementById('answer');
+let answerInput = document.getElementById('answer');
+const KEY = 'trnsl.1.1.20181105T164937Z.ee522e94c8af9c25.2938b6eb1e9eecf9fcd356cb37a55bcb85964cbd';
 let i;
+let correctCount = 0;
+let incorrectWords = [];
+
+nextBtn.addEventListener("click", submit);
+
+(function() {
+    nextWord();
+})();
 
 
-nextBtn.addEventListener("click", newWord);
-window.onload = function(){
-    newWord();
+
+async function submit() {
+    const word = question.innerHTML.toLowerCase();    // question
+    const translation = await getTranslation(word);   // answer
+    let correct = isCorrect(translation);   // check if the answer is correct
+
+    if (correct) {
+        // increment number of correct answers
+        correctCount++;
+        showToast(true);
+    } else {
+        // save word that was incorrect
+        incorrectWords.push({
+            question: word,
+            answer: translation
+        });
+        showToast(false);
+    }
+    nextWord();
 }
 
-function newWord() {
+function getTranslation(word) {
+    const URL = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${KEY}&text=${word}&lang=fr-en`;
+    return fetch(URL).then(function(response) {
+        return response.json().then(function(data) {
+            if (data.code == 200) {
+                return data.text[0].toLowerCase();
+            }
+        });
+    });
+}
+
+function isCorrect(translated) {
+    const answer = answerInput.value.trim().toLowerCase();    // get user input answer
+    let _translated = translated.split(' ');
+    let flag = false;
+    _translated.forEach(function(word) {
+        if (translated === answer || word === answer) {
+            flag = true;
+        }
+    })
+    return flag;
+}
+
+function nextWord() {
+    // clear user input 
+    answerInput.value = "";
     i = Math.floor(Math.random() * words.length - 1);
     question.innerHTML = words[i].q;
-    answer.innerHTML = words[i].a;
     fadeIn(question);
 }
 
+function showToast(correct) {
+    let toast;
+    if (correct) {
+        toast = document.getElementById('correct');
+    } else {
+        toast = document.getElementById('wrong');
+    }
+    
+    // animate toast
+    toast.classList.add('active');
+    setTimeout(function() {
+        toast.classList.remove('active');
+    }, 1500);    
+}
 
-let dataset = [];
+//TODO: load this dataset
 
-Promise.all([
-        // "/../french-words/french-word-list-adjectives.csv",
-        "/../french-words/french-word-list-nouns.csv",
-        "/../french-words/french-word-list-verbs.csv",
-    ].map(url => d3.csv(url))).then(function(values) {
-    let data = values[0];
+// let dataset = [];
+// Promise.all([
+//         // "/../french-words/french-word-list-adjectives.csv",
+//         "/../french-words/french-word-list-nouns.csv",
+//         "/../french-words/french-word-list-verbs.csv",
+//     ].map(url => d3.csv(url))).then(function(values) {
+//     let data = values[0];
 
-    data.forEach(entry=>{
-        dataset.push(entry["language:"]);
-    })
+//     data.forEach(entry=>{
+//         dataset.push(entry["language:"]);
+//     })
 
-    // dataset.sort((a,b)=> a>b ? 1 : a<b ? -1 : 0);
-    // console.log(dataset);
-});
+//     // dataset.sort((a,b)=> a>b ? 1 : a<b ? -1 : 0);
+//     // console.log(dataset);
+// });
 
 
 
--- js/translate.js
+++ js/translate.js
@@ -1,12 +1,12 @@
 // French: fr
 const KEY = 'trnsl.1.1.20181105T164937Z.ee522e94c8af9c25.2938b6eb1e9eecf9fcd356cb37a55bcb85964cbd';
-let URL;
-
-URL = `https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=${KEY}&ui=fr`;
 
 
-function translateListener() {
+async function translate(word) {
+    const URL = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${KEY}&text=${word}&lang=en-fr&format=html`;
     fetch(URL).then(function(response) {
-        console.log(response);
+        return Promise.resolve(response);
+    }).catch(function(error) {
+        return Promise.reject(error);
     });
 };
